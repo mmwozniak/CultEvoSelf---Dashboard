59a019dd42b8dc43fdb4d54c60a36e7fe30dc887
mmwozniak <mgwozniak@gmail.com>
2023-06-08 17:44:39 +0200

    Update Dashboard_CultEvoSelf1.py

--- Dashboard_CultEvoSelf1.py
+++ Dashboard_CultEvoSelf1.py
@@ -489,7 +489,7 @@ if button_radio == 'Analysis of individual traits':
     # Lineplot + scatter (stripplot)
     #ax1 = sns.lineplot(x='id_exp_participant', y='Avg_'+trait, data=df_lmm2, hue='group') #, err_style='band', ci=95, palette = ['g', 'r'] ) # also: col, row ; ,x_jitter=0, truncate - limit the data to min-max
     #ax1 = sns.stripplot(x="id_exp_participant", y='Avg_'+trait, data=df_lmm2, hue='group', palette = ['g', 'r'] )
-    ax1.get_legend().remove()
+   #ax1.get_legend().remove()
     ax1.set(title='Trait: '+trait_labels[trait], xlabel='Generation', ylabel='FOT [%]')
     
     # Add median line
